b4fefc776582dd5d33946072dae701dfc69d823f
Bricked Bozo <brickedbozo@proton.me>
2024-12-03 16:57:24 +0100

    small changes to d_03

--- d_03/src/main.rs
+++ d_03/src/main.rs
@@ -1,67 +1,69 @@
 #![allow(dead_code, unused)]
-
 use std::fs;
 fn main() {
     let mut instruction = true;
     let mut res = 0;
     let f = fs::read_to_string("i2").unwrap();
+    f.replace("\n", "");
     let fl = f.len();
     let mut i = 0;
     'main: loop {
+        if i == fl - 1 {
+            break;
+        }
         if i + 4 < fl {
             let phrase = &f[i..i + 4];
             if phrase == "do()" {
                 instruction = true;
+                i += 4;
+                continue 'main;
             }
         }
-
         if i + 7 < fl {
             let phrase = &f[i..i + 7];
             if phrase == "don't()" {
                 instruction = false;
+                i += 7;
+                continue 'main;
             }
         }
         if i + 4 < fl {
             let phrase = &f[i..i + 4];
             if phrase == "mul(" {
                 i += 4;
-                let mut numbers1 = String::new();
+                let mut numbers = String::new();
                 loop {
                     if i < fl {
                         let c = f.chars().nth(i).unwrap();
                         if c.is_ascii_digit() {
-                            numbers1.push(c);
+                            numbers.push(c);
                             i += 1;
                             continue;
                         }
                         if c == ',' {
                             i += 1;
                             break;
-                        } else {
-                            continue 'main;
                         }
+                        continue 'main;
                     }
-                    continue 'main;
                 }
-                let c = f.chars().nth(i).unwrap();
-                let mut numbers2 = String::new();
+                let n1 = parse_str_to_number(&numbers);
+                numbers.clear();
                 loop {
                     if i < fl {
                         let c = f.chars().nth(i).unwrap();
                         if c.is_ascii_digit() {
-                            numbers2.push(c);
+                            numbers.push(c);
                             i += 1;
                             continue;
                         }
                         if c == ')' {
                             break;
                         }
+                        continue 'main;
                     }
-                    continue 'main;
                 }
-
-                let n1 = parse_str_to_number(&numbers1);
-                let n2 = parse_str_to_number(&numbers2);
+                let n2 = parse_str_to_number(&numbers);
                 if instruction {
                     res += n1 * n2;
                 }
