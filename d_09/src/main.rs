d1740d1e95aae7a1507f3f95acd626e602b81908
Bricked Bozo <brickedbozo@proton.me>
2024-12-10 15:19:37 +0100

    day 9 part 2 done

--- d_09/src/main.rs
+++ d_09/src/main.rs
@@ -1,11 +1,68 @@
 #![allow(dead_code, unused, clippy::all)]
-
-use std::fs;
+use std::{collections::HashSet, fs};
 fn main() {
     let inp = fs::read_to_string("i2").unwrap();
     let p1 = calc_p1(&inp);
     println!("part1: {p1}");
-    // 90950452031
+    let p2 = calc_p2(&inp);
+    println!("part2: {p2}");
+}
+fn calc_p2(inp: &str) -> u64 {
+    let mut res = 0;
+    let mut disk = parse_input(inp);
+    sort_disk2(&mut disk);
+    let mut res = 0;
+    disk.into_iter().enumerate().for_each(|(i, e)| match e {
+        Some(n) => {
+            res += i as u64 * n;
+        }
+        None => {}
+    });
+    res
+}
+fn sort_disk2(disk: &mut [Option<u64>]) {
+    let mut tail = disk.len() - 1;
+    while tail > 0 {
+        match disk[tail] {
+            Some(id) => {
+                let mut id_len = 1;
+                for j in (0..tail).rev() {
+                    if let Some(n) = disk[j] {
+                        if n == id {
+                            id_len += 1;
+                            tail -= 1;
+                            continue;
+                        }
+                    }
+                    break;
+                }
+                let mut dot_len = 0;
+                for start in 0..tail {
+                    if disk[start].is_none() {
+                        dot_len += 1;
+                    } else {
+                        dot_len = 0;
+                        continue;
+                    }
+                    if dot_len == id_len {
+                        for k in start - dot_len + 1..=start {
+                            disk[k] = Some(id);
+                        }
+                        for k in tail..=tail + id_len - 1 {
+                            disk[k] = None;
+                        }
+                        break;
+                    }
+                }
+            }
+            None => {}
+        }
+        if tail > 0 {
+            tail -= 1;
+        } else {
+            break;
+        }
+    }
 }
 fn calc_p1(inp: &str) -> u64 {
     let mut res = 0;
@@ -35,7 +92,6 @@ fn sort_disk(disk: &mut [Option<u64>]) {
         }
     }
 }
-
 fn parse_input(inp: &str) -> Vec<Option<u64>> {
     let inp = &inp[..inp.len() - 1];
     let mut res = vec![];
